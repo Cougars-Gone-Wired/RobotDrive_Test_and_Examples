a5857543e88100cbb8bf0a9f8917f89cd24167a7
tiegu <tiegu@192.168.0.9>
2018-01-01 23:14:18 -0700

    removed an uneccesary line break

--- RobotDriveTest/src/howCodeWorks/Caller.java
+++ RobotDriveTest/src/howCodeWorks/Caller.java
@@ -51,7 +51,7 @@ public class Caller {
 		Mecanum_Cartesian.mecanumDrive_Cartesian(mecanum_X_Axis_Invert * (mecanum_LS_X_Axis), mecanum_Y_Axis_Invert * (mecanum_LS_Y_Axis), mecanum_Rotate_Axis_Invert * (mecanum_RS_X_Axis), 0.0);
 		System.out.println("Mecanum Drive:");
 		System.out.println(RobotDrive.m_frontLeftMotor + "  " + RobotDrive.m_frontRightMotor);
-		System.out.println(RobotDrive.m_rearLeftMotor + "  " + RobotDrive.m_rearRightMotor + "\n");
+		System.out.println(RobotDrive.m_rearLeftMotor + "  " + RobotDrive.m_rearRightMotor);
 	}
 
 }
